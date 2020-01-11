3bef8383ff96324d8749dd3b04d3e61c4636c959
Gian Umerez <gianpaoloumerez@gmail.com>
2020-01-11 20:10:45 +0800

    Button 0 and 1 Working

--- Form1.vb
+++ Form1.vb
@@ -10,4 +10,19 @@
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
 
     End Sub
+
+    Private Sub Button4_MouseClick(sender As Object, e As MouseEventArgs) Handles Button4.MouseClick
+        If Label1.Text <> "0" Then
+            Label1.Text += "0"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If Label1.Text = "0" Then
+            Label1.Text = "1"
+        ElseIf Label1.Text <> "0" Then
+            Label1.Text += "1"
+        End If
+
+    End Sub
 End Class
