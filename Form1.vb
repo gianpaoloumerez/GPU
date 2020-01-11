5e0679024888154016a1e5ba88c4e123a0f6f3be
Gian Umerez <gianpaoloumerez@gmail.com>
2020-01-11 19:52:26 +0800

    UI no Function

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Calculator
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+End Class
